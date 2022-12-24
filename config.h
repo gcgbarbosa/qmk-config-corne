b232f2928298c5d25c78851555f334037c635455
George C. G. Barbosa <gcgbarbosa@gmail.com>
2022-12-24 13:37:06 -0700

    Update config.h

--- config.h
+++ config.h
@@ -1,3 +1,17 @@
+#include "config_common.h"
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS  8
+#define MATRIX_COLS  6
+#define MATRIX_ROW_PINS \
+    { D4, C6, D7, E6 }
+
+// wiring of each half
+#define MATRIX_COL_PINS \
+    { F4, F5, F6, F7, B1, B3 }
+
+
 // Configure the global tapping term (default: 150ms)
 #define TAPPING_TERM 150
 
