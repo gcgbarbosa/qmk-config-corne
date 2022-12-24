5f5668c70cf61e139077b914965e35141deeb8b5
George C. G. Barbosa <gcgbarbosa@gmail.com>
2022-12-24 13:35:12 -0700

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -6,14 +6,9 @@ SRC += source.c
 # Bootloader selection
 # BOOTLOADER = caterina
 
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-
-AUDIO_ENABLE = no           # Audio output
 
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
