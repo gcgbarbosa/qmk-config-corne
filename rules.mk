c711bc5df316ec29fc636547e511b2077c7c9c51
George C. G. Barbosa <gcgbarbosa@gmail.com>
2022-12-24 13:44:36 -0700

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -13,8 +13,8 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
+# RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_DRIVER = WS2812
 
 LTO_ENABLE = yes # compress 
 
