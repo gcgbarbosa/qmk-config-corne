75323ed26276eb81427ca86b575ec711dd0a6568
George C. G. Barbosa <gcgbarbosa@gmail.com>
2022-12-24 20:35:45 -0700

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -8,10 +8,10 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no
 RGBLIGHT_DRIVER = WS2812
 
-RGB_MATRIX_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 
 OLED_ENABLE     = yes
