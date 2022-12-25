8eb9d8ec3959d0514253e67bd8438432a6bbad1d
George C. G. Barbosa <gcgbarbosa@gmail.com>
2022-12-24 19:27:54 -0700

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,26 +1,22 @@
 SRC += source.c
 
-# MCU name
-# MCU = atmega32u4
-
-# Bootloader selection
-# BOOTLOADER = caterina
+SPLIT_KEYBOARD = yes
+RGBLIGHT_SUPPORTED = yes
+RGB_MATRIX_SUPPORTED = yes
 
 MOUSEKEY_ENABLE = yes       # Mouse keys
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 
-#BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
-#RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes
+RGBLIGHT_DRIVER = WS2812
 
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
-
-LTO_ENABLE = yes # compress 
 
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 
-SPLIT_KEYBOARD = yes
-RGBLIGHT_SUPPORTED = yes
-RGB_MATRIX_SUPPORTED = yes
+LTO_ENABLE = yes # compress 
+
+
