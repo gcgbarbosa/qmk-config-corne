0fab7ac6fbedd5c1cfc6b2e266c570bde8301f3b
George C. G. Barbosa <gcgbarbosa@gmail.com>
2023-02-17 14:57:16 -0700

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,16 +1,16 @@
 SRC += source.c
 
 SPLIT_KEYBOARD = yes
-RGBLIGHT_SUPPORTED = yes
-RGB_MATRIX_SUPPORTED = yes
 
 MOUSEKEY_ENABLE = yes       # Mouse keys
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 
+RGBLIGHT_SUPPORTED = no
 RGBLIGHT_ENABLE = no
 RGBLIGHT_DRIVER = WS2812
 
+RGB_MATRIX_SUPPORTED = yes
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 
