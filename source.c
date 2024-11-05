40056da52fb4fa2e86c0ed03b4b5e4bf59da3607
George C. G. Barbosa <gcgbarbosa@gmail.com>
2024-11-05 16:56:53 +0000

    Update source.c

--- source.c
+++ source.c
@@ -1,5 +1,17 @@
 #include QMK_KEYBOARD_H
 
+enum unicode_names {
+    BANG,
+    IRONY,
+    SNEK
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [BANG]  = 0x203D,  // ‽
+    [IRONY] = 0x2E2E,  // ⸮
+    [SNEK]  = 0x1F40D, // 🐍
+};
+
 
 #ifdef OLED_ENABLE
 
