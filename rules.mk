1c63362be5ff85f24f68fe95d3625fa8ff798144
George C. G. Barbosa <gcgbarbosa@gmail.com>
2024-11-05 16:34:00 +0000

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -6,16 +6,17 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 
+# emoji support
+UNICODE_COMMON = yes
+
 RGBLIGHT_SUPPORTED = no
 RGBLIGHT_ENABLE = no
-# RGBLIGHT_DRIVER = WS2812
 
 RGB_MATRIX_SUPPORTED = yes
 RGB_MATRIX_ENABLE = yes
-# RGB_MATRIX_DRIVER = WS2812
 
 OLED_ENABLE     = yes
-# OLED_DRIVER     = SSD1306
+
 
 LTO_ENABLE = yes # compress 
 EXTRAFLAGS += -flto # god please help
