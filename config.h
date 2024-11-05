cb16d3eea5c7c1412f49f52254a08060e0eebcec
George C. G. Barbosa <gcgbarbosa@gmail.com>
2024-11-05 17:20:31 +0000

    Update config.h

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* set unicode mode to windows */
-#define UNICODE_SELECTED_MODES UC_WINC
+#define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE, UNICODE_MODE_LINUX
 
 /* lighting optoins */
 #define RGBLED_NUM       54 // Number of LEDs
