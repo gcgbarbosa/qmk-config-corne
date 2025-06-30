a8f7389af6a8fdcb25eb8673eaca2d232c48c25d
George C. G. Barbosa <gcgbarbosa@gmail.com>
2025-06-30 15:28:12 +0200

    Update source.c

--- source.c
+++ source.c
@@ -27,21 +27,6 @@ const uint32_t PROGMEM unicode_map[] = {
     [RIGHT]  = 0x1F449,  // 👉🏿9
 };
 
-enum my_keycodes {
- // apple globe key
- AP_GLOB = SAFE_RANGE,
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-  case AP_GLOB:
-    host_consumer_send(record->event.pressed ? AC_NEXT_KEYBOARD_LAYOUT_SELECT : 0);
-    return false;
-  }
-
-  return true;
-}
-
 #ifdef OLED_ENABLE
 
 static void render_logo(void) {
